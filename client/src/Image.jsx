7a16217d0300f12227a128656590e4c8a09c57a9
[mohit kota] <indianstar725@gmail.com>
2023-09-28 00:05:13 +0530

    server deployed on render

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'http://localhost:3000/uploads/'+src;
+      : 'https://airbnbserver-5832.onrender.com/uploads/'+src;
     return (
       <img {...rest} src={src} alt={''} />
     );
